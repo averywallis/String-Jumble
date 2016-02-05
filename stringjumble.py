ae00ac72a388d1f11bdfc537dc3c2e1b932175cf
averywallis <averybwallis@gmail.com>
2016-02-05 14:55:39 -0500

    Updated from Brython Server: 2/5/2016 2:55:39 PM

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
 Author: Avery Wallis
-Credit: <sources>
+Credit: None so far
 
 Assignment:
 
@@ -25,3 +25,5 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+string= str(input("Please enter a string of text (the bigger the better): "))
+
