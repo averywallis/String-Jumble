aed4330d2c2286940488e926e660fec1439fec03
averywallis <averybwallis@gmail.com>
2016-02-06 16:20:36 -0500

    Updated from Brython Server: 2/6/2016 4:20:37 PM

--- stringjumble.py
+++ stringjumble.py
@@ -26,6 +26,5 @@ handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 string= str(input("Please enter a string of text (the bigger the better): "))
+print(len(string))
 
-
-print(string[[-1-2]])
