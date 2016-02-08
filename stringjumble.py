28642dcdd2b7af59fdeb3ea7911e28917e37a741
averywallis <averybwallis@gmail.com>
2016-02-08 14:11:56 -0500

    Updated from Brython Server: 2/8/2016 2:11:56 PM

--- stringjumble.py
+++ stringjumble.py
@@ -28,4 +28,4 @@ erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 string= str(input("Please enter a string of text (the bigger the better): "))
 l=(int(len(string)))
 
-print(string(l)l in range(-(l-(l-1))])
+print(ll in range(-(l-(l-1))])
