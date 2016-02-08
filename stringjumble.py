6796e21e49439bfe8bb5765519da3233210d25dd
averywallis <averybwallis@gmail.com>
2016-02-08 14:35:25 -0500

    Updated from Brython Server: 2/8/2016 2:35:25 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,13 +25,13 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
-string= str(input("Please enter a string of text (the bigger the better): "))
+string= list(str(input("Please enter a string of text (the bigger the better): ")))
 l=(int(len(string)))
-
+"""
 while -l in range(-(l-(l-1)),-l):
     print(y,end="")
 print(string[-(l-(l-1))])
-"print(string[l in range(-(l-(l-1))),-l])"
+print(string[l in range(-(l-(l-1))),-l])
+"""
 for x in range(1,l):
     print(string[-x],end="")
-print(string[-l])
