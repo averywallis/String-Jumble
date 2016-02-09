ca7c9e4907bea9586326763d74e2c1e13ebd3252
averywallis <averybwallis@gmail.com>
2016-02-09 14:54:45 -0500

    Updated from Brython Server: 2/9/2016 2:54:45 PM

--- stringjumble.py
+++ stringjumble.py
@@ -38,7 +38,7 @@ for x in range(l,1,-1):
     if string[-x]==" ":
         for y in range(l,x,-1):
             print(string[-y],end="")
-            a={0]
+            a={0}
         
 """
 for x in range(l,1,-1):
