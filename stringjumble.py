5090637784adf5d3e5db0fb3adcb0819f60da974
averywallis <averybwallis@gmail.com>
2016-02-12 14:42:51 -0500

    Updated from Brython Server: 2/12/2016 2:42:51 PM

--- stringjumble.py
+++ stringjumble.py
@@ -47,9 +47,3 @@ for words in spil:
     while g>0:
         print(word,end="")
         g=g+1
-"""
-for x in range(l,1,-1):
-    if string[-x]==" ":
-        for y in range(l,x-1,1):
-            print(string[-y+(l-x)],end="")
-"""
